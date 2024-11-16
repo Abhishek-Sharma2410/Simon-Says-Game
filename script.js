569961e32ccc8d6ac9ddf44b3d45fe0f6bba91f4
Abhishek-Sharma2410 <asabhi.sharma165@gmail.com>
2024-11-16 20:02:39 +0530

    Added level sequences and Flash Buttons

--- script.js
+++ script.js
@@ -8,3 +8,63 @@ let close = document.getElementById("close-button");
 close.addEventListener("click", ()=>{
     otherDiv.classList.toggle("active");
 })
+
+
+let level = 0;
+let colorSequence = ["green", "red", "blue", "yellow"];
+let gameSequence = [];
+let userSequence = [];
+let heading = document.getElementById("level");
+document.addEventListener("keypress", ()=>{
+  if(level==0) {
+    level++;
+    changetextAndLevel(level); 
+    console.log(level);
+    let color = chooseRandom(colorSequence);
+    let button = document.getElementById(`${color}`);
+    flashButton(button);
+    gameSequence.push(color);
+    console.log(gameSequence);
+    }  
+})
+
+let greenButton = document.getElementById("green");
+let redButton = document.getElementById("red");
+let blueButton = document.getElementById("blue");
+let yellowButton = document.getElementById("yellow");
+
+greenButton.addEventListener("click", ()=>{
+    userSequence.push("green");
+    let button = document.getElementById("green");
+    flashButton(button);
+})
+redButton.addEventListener("click", ()=>{
+    userSequence.push("red");
+    let button = document.getElementById("red");
+    flashButton(button);
+})
+blueButton.addEventListener("click", ()=>{
+    userSequence.push("blue");
+    let button = document.getElementById("blue");
+    flashButton(button);
+})
+yellowButton.addEventListener("click", ()=>{
+    userSequence.push("yellow");
+    let button = document.getElementById("yellow");
+    flashButton(button);
+})  
+
+function changetextAndLevel(level){
+    heading.innerText = `Level ${level}`;
+}
+
+let chooseRandom = (colorSequence)=>{
+    return colorSequence[Math.floor(Math.random()*4)];
+}
+
+let flashButton = (divId)=>{
+    divId.classList.add("flash");
+    setTimeout(()=>{
+        divId.classList.remove("flash");
+    },200)
+}
